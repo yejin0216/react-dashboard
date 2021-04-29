06a2bebd42b8f34e9d609e621c90d9578e629fec
yejin <yejin0216@gmail.com>
2021-04-29 11:50:17 +0900

    util 함수 제거

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -1,37 +1,42 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import * as authApi from 'apis/auth';
 import { LOGIN, LOGOUT } from 'modules/utils/actionType';
-import { asyncResponseType } from 'modules/utils/asyncUtils';
+import * as actions from 'modules/auth';
 import history from 'history';
 
 // 로그인
 function* loginSaga({ payload }) {
   try {
+    // 로그인은 동기로 처리
     const { data } = yield call(authApi.login, payload);
     const { failCount } = data;
 
     if ( failCount || failCount > 0 ) {
-      yield put(asyncResponseType.error(LOGIN, 'INVALID_USER'));
+      // 로그인 실패 횟수 카운트
+      // 5회 이상 실패할 경우 비밀번호 초기화 됨
+      yield put(actions.loginErrorAction('INVALID_USER'));
     } else {
       localStorage.setItem('access_token', data.access_token);
       localStorage.setItem('target_ids', data.target_ids);
 
-      yield put(asyncResponseType.success(LOGIN, data));
+      yield put(actions.loginSuccessAction(data));
       history.push('/');
     }
   } catch (e) {
-    yield put(asyncResponseType.error(LOGIN, e));
+    yield put(actions.loginErrorAction(e));
   }
 };
 
 // 로그아웃
 function* logoutSaga() {
   yield call(authApi.logout);
-  yield put(asyncResponseType.default(LOGOUT));
+  yield put(actions.logoutAction);
   history.push('/auth');
 }
 
+// 액션이 디스패치되면 실행된다.
 export default function* authSaga() {
+  // 로그인, 로그아웃은 디바운스로 처리
   yield takeLatest(LOGIN, loginSaga);
-  yield takeLatest(LOGIN, logoutSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 };
