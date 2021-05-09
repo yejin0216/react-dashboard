368d32988ea7ff0f0dfb53bc381897e67f5e7405
yejin <yejin0216@gmail.com>
2021-05-09 12:21:16 +0900

    api uri 오류 수정

--- src/apis/common.js
+++ src/apis/common.js
@@ -2,16 +2,16 @@ import { AUTH_AXIOS } from 'axios.config';
 import { HOST_CODES_API, HOST_OPENSERVICE_API } from './constants';
 
 // Open Service 공통코드 조회
-export const getOpenCommonCodes = async (codeId) => {
+export const getOpenCommonCodes = async codeId => {
   const language = localStorage.getItem('language') || 'KOR';
   return await AUTH_AXIOS.get(
-    `${HOST_OPENSERVICE_API}/codes/${codeId}?language=${language}`
+    `${HOST_OPENSERVICE_API}/codes/${codeId}?language=${language}`,
   );
 };
 
 // Dashboard 공통코드 조회 (카테고리)
 export const getCategories = async () => {
-  return await AUTH_AXIOS.get(`${HOST_CODES_API}/codes/categories`);
+  return await AUTH_AXIOS.get(`${HOST_CODES_API}/categories`);
 };
 
 // FAQ 목록 조회
