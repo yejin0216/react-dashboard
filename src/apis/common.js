b69c1ef959a31efe73651f3c98e7efdd64c126e8
yejin <yejin0216@gmail.com>
2021-05-14 21:40:10 +0900

    공통코드는 useCache 사용하도록 변경

--- src/apis/common.js
+++ src/apis/common.js
@@ -6,12 +6,17 @@ export const getOpenCommonCodes = async codeId => {
   const language = localStorage.getItem('language') || 'KOR';
   return await AUTH_AXIOS.get(
     `${HOST_OPENSERVICE_API}/codes/${codeId}?language=${language}`,
+    {
+      useCache: true,
+    },
   );
 };
 
 // Dashboard 공통코드 조회 (카테고리)
 export const getCategories = async () => {
-  return await AUTH_AXIOS.get(`${HOST_CODES_API}/categories`);
+  return await AUTH_AXIOS.get(`${HOST_CODES_API}/categories`, {
+    useCache: true,
+  });
 };
 
 // FAQ 목록 조회
@@ -19,5 +24,8 @@ export const getFAQs = async () => {
   const language = localStorage.getItem('language') || 'KOR';
   return await AUTH_AXIOS.get(
     `${HOST_OPENSERVICE_API}/bbs/0002/0004?pageNum=1&pageCon=15&langCd=${language}`,
+    {
+      useCache: true,
+    },
   );
 };
