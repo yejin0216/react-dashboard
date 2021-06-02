7869dc6983dd1a75aedc2ea87b679062a4bba63c
yejin <yejin0216@gmail.com>
2021-06-02 20:53:52 +0900

    breadcrumb 없을 경우 rendering 안하도록 변경함

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
 function Header({ breadcrumb }) {
+  if (breadcrumb === undefined) {
+    return null;
+  }
   return (
     <div className="wm-top-header">
       <div className="page-title">
         <h1 className="h1">{breadcrumb[0]}</h1>
-        <ul className="breadcumb">
-          <li>
-            <a href="#none">{breadcrumb[1]}</a>
-          </li>
-          <li className="sep">{'>'}</li>
-          <li>
-            <a href="#none">{breadcrumb[0]}</a>
-          </li>
-        </ul>
+        {breadcrumb[1] && (
+          <ul className="breadcumb">
+            <li>
+              <a href="#none">{breadcrumb[1]}</a>
+            </li>
+            <li className="sep">{'>'}</li>
+            <li>
+              <a href="#none">{breadcrumb[0]}</a>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
